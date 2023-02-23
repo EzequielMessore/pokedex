3b6c54a184d7c71ee6ae0c07b4cf97ffb8c93571
Ezequiel Messore <ezequiel.messore@hotmail.com>
2023-02-22 21:02:03 -0300

    Adding Android Project

---
+++ app/src/main/java/br/com/messore/tech/pokedex/MainActivity.kt
@@ -0,0 +1,40 @@
+package br.com.messore.tech.pokedex
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.messore.tech.pokedex.ui.theme.PokédexTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokédexTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DefaultPreview() {
+    PokédexTheme {
+        Greeting("Android")
+    }
+}
