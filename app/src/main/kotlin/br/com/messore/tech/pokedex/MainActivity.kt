5e8026928a95ae95d9f2f1fc3abdcb88e0cf2dc8
EzequielMessore <ezequiel.messore@hotmail.com>
2023-03-10 22:08:10 -0300

    Adding search bar and filters.

--- app/src/main/kotlin/br/com/messore/tech/pokedex/MainActivity.kt
+++ app/src/main/kotlin/br/com/messore/tech/pokedex/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import br.com.messore.tech.pokedex.pokelist.PokeListScreen
+import br.com.messore.tech.pokedex.pokelist.Pokemon
+import br.com.messore.tech.pokedex.pokelist.R
 import br.com.messore.tech.pokedex.ui.theme.PokedexTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +22,37 @@ class MainActivity : ComponentActivity() {
             PokedexTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    PokeListScreen()
+                    PokeListScreen(createMock())
                 }
             }
         }
+    }
+
+    private fun createMock(): List<Pokemon> {
+        val pokemonTypes = listOf(
+            Pokemon.Type(
+                text = "Grama",
+                color = Color(0xff63bc5a),
+                iconResource = R.drawable.ic_type_grass,
+            ),
+            Pokemon.Type(
+                text = "Venenoso",
+                color = Color(0xffb567ce),
+                iconResource = R.drawable.ic_type_poison,
+            ),
+        )
+        val bulbasaur = Pokemon(
+            1,
+            "Bulbasaur",
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+            pokemonTypes,
+        )
+        val ivysaur = Pokemon(
+            2,
+            "Ivysaur",
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+            pokemonTypes,
+        )
+        return listOf(bulbasaur, ivysaur)
     }
 }
--- features/pokelist/src/main/kotlin/br/com/messore/tech/pokedex/pokelist/PokeListScreen.kt
+++ features/pokelist/src/main/kotlin/br/com/messore/tech/pokedex/pokelist/PokeListScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,10 +19,20 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,42 +48,76 @@ import coil.compose.AsyncImage
 
 @Composable
 @ExperimentalMaterial3Api
-fun PokeListScreen() {
-    val pokemonTypes = listOf(
-        Pokemon.Type(
-            text = "Grama",
-            color = Color(0xff63bc5a),
-            iconResource = R.drawable.ic_type_grass,
-        ),
-        Pokemon.Type(
-            text = "Venenoso",
-            color = Color(0xffb567ce),
-            iconResource = R.drawable.ic_type_poison,
-        ),
-    )
-    val bulbasaur = Pokemon(
-        1,
-        "Bulbasaur",
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-        pokemonTypes,
-    )
-    val ivysaur = Pokemon(
-        2,
-        "Ivysaur",
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-        pokemonTypes,
-    )
-    val pokemonList = listOf(bulbasaur,ivysaur)
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        items(pokemonList) {pokemon->
-            PokeItem(pokemon)
+fun PokeListScreen(pokemonList: List<Pokemon> = listOf()) {
+    Column {
+        Search()
+
+        Filters()
+
+        LazyColumn(
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            items(pokemonList) { pokemon ->
+                PokeItem(pokemon)
+            }
         }
     }
-
 }
 
 @Composable
-fun PokeItem(pokemon:Pokemon) {
+@ExperimentalMaterial3Api
+fun Search() {
+    var text by rememberSaveable { mutableStateOf("") }
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        value = text,
+        placeholder = { Text(text = "Procurar Pókemon...") },
+        leadingIcon = {
+            Icon(
+                modifier = Modifier.padding(start = 6.dp),
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        onValueChange = { text = it },
+        shape = CircleShape
+    )
+}
+
+const val DarkGray = 0xff333333
+
+@Composable
+fun Filters(defaultColor: Color = Color(DarkGray)) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Button(
+            colors = ButtonDefaults.buttonColors(containerColor = defaultColor),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "Todos os tipos")
+            Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = null)
+        }
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = defaultColor),
+        ) {
+            Text(text = "Menor número")
+            Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = null)
+        }
+    }
+}
+
+@Composable
+fun PokeItem(pokemon: Pokemon) {
     Row(
         modifier = Modifier
             .aspectRatio(3.22f)
@@ -88,11 +133,11 @@ fun PokeItem(pokemon:Pokemon) {
             verticalArrangement = Arrangement.SpaceBetween,
         ) {
             Text(
-                text = "Nº${pokemon.id.toString().padStart(3,'0')}",
+                text = "Nº${pokemon.id.toString().padStart(3, '0')}",
                 style = TextStyle(
                     fontSize = 12.sp,
                     fontWeight = FontWeight.W600,
-                    color = Color(0xff333333),
+                    color = Color(DarkGray),
                 ),
             )
             Text(
@@ -196,5 +241,39 @@ data class Pokemon(
 @Composable
 @Preview(showBackground = true)
 fun DefaultPreview() {
-    PokeListScreen()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        PokeListScreen(createMock())
+    }
+}
+
+private fun createMock(): List<Pokemon> {
+    val pokemonTypes = listOf(
+        Pokemon.Type(
+            text = "Grama",
+            color = Color(0xff63bc5a),
+            iconResource = R.drawable.ic_type_grass,
+        ),
+        Pokemon.Type(
+            text = "Venenoso",
+            color = Color(0xffb567ce),
+            iconResource = R.drawable.ic_type_poison,
+        ),
+    )
+    val bulbasaur = Pokemon(
+        1,
+        "Bulbasaur",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        pokemonTypes,
+    )
+    val ivysaur = Pokemon(
+        2,
+        "Ivysaur",
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+        pokemonTypes,
+    )
+    return listOf(bulbasaur, ivysaur)
 }
