0048cfce2190af5020636015f629036d92889dac
EzequielMessore <ezequiel.messore@hotmail.com>
2023-04-19 11:42:38 -0300

    Adding compose-material . Adding kotlin options to ignore Experimental.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ allprojects {
     tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class).all {
         kotlinOptions {
             freeCompilerArgs = freeCompilerArgs + listOf(
+                "-Xopt-in=androidx.compose.material.ExperimentalMaterialApi",
                 "-Xopt-in=androidx.compose.material3.ExperimentalMaterial3Api",
             )
         }
